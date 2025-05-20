12189cb772c22ab0f96efb11dc64d040a2bd213e
sahana426 <sahanasahana65867@gmail.com>
2025-05-20 23:26:53 +0530

    Add files via upload

---
+++ 8b.jsp-coffee-update/test12 (1).sql
@@ -0,0 +1,71 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: May 20, 2025 at 04:27 PM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `test12`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `coffee`
+--
+
+CREATE TABLE `coffee` (
+  `id` int(11) NOT NULL,
+  `coffee_name` varchar(255) DEFAULT NULL,
+  `price` decimal(10,2) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `coffee`
+--
+
+INSERT INTO `coffee` (`id`, `coffee_name`, `price`) VALUES
+(1, 'latte', 180.00),
+(2, 'espresso', 240.00),
+(3, 'royal', 200.00),
+(4, 'cappacinoo', 180.00),
+(5, 'gold', 180.00),
+(6, 'royal', 200.00);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `coffee`
+--
+ALTER TABLE `coffee`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `coffee`
+--
+ALTER TABLE `coffee`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
